eacabadd32625dd85d7993d74df894fc94c2bdb7
gioev.arsen15@list.ru <gioev.arsen15@list.ru>
2023-01-09 11:41:58 +0300

    Добавьте файлы проекта.

---
+++ Timetable/FormTimetable.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Timetable
+{
+    partial class FormTimetable
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelWeekDay = new System.Windows.Forms.Label();
+            this.listBoxTimetable = new System.Windows.Forms.ListBox();
+            this.checkBoxMonday = new System.Windows.Forms.CheckBox();
+            this.checkBoxTuesday = new System.Windows.Forms.CheckBox();
+            this.checkBoxWednesday = new System.Windows.Forms.CheckBox();
+            this.checkBoxThursday = new System.Windows.Forms.CheckBox();
+            this.checkBoxFriday = new System.Windows.Forms.CheckBox();
+            this.checkBoxSaturday = new System.Windows.Forms.CheckBox();
+            this.checkBoxSunday = new System.Windows.Forms.CheckBox();
+            this.buttonShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelWeekDay
+            // 
+            this.labelWeekDay.AutoSize = true;
+            this.labelWeekDay.Location = new System.Drawing.Point(13, 27);
+            this.labelWeekDay.Name = "labelWeekDay";
+            this.labelWeekDay.Size = new System.Drawing.Size(73, 13);
+            this.labelWeekDay.TabIndex = 0;
+            this.labelWeekDay.Text = "День недели";
+            // 
+            // listBoxTimetable
+            // 
+            this.listBoxTimetable.FormattingEnabled = true;
+            this.listBoxTimetable.Location = new System.Drawing.Point(157, 27);
+            this.listBoxTimetable.Name = "listBoxTimetable";
+            this.listBoxTimetable.Size = new System.Drawing.Size(204, 290);
+            this.listBoxTimetable.TabIndex = 1;
+            // 
+            // checkBoxMonday
+            // 
+            this.checkBoxMonday.AutoSize = true;
+            this.checkBoxMonday.Location = new System.Drawing.Point(16, 62);
+            this.checkBoxMonday.Name = "checkBoxMonday";
+            this.checkBoxMonday.Size = new System.Drawing.Size(94, 17);
+            this.checkBoxMonday.TabIndex = 2;
+            this.checkBoxMonday.Text = "Понедельник";
+            this.checkBoxMonday.UseVisualStyleBackColor = true;
+            this.checkBoxMonday.CheckedChanged += new System.EventHandler(this.checkBoxMonday_CheckedChanged);
+            // 
+            // checkBoxTuesday
+            // 
+            this.checkBoxTuesday.AutoSize = true;
+            this.checkBoxTuesday.Location = new System.Drawing.Point(16, 96);
+            this.checkBoxTuesday.Name = "checkBoxTuesday";
+            this.checkBoxTuesday.Size = new System.Drawing.Size(68, 17);
+            this.checkBoxTuesday.TabIndex = 3;
+            this.checkBoxTuesday.Text = "Вторник";
+            this.checkBoxTuesday.UseVisualStyleBackColor = true;
+            this.checkBoxTuesday.CheckedChanged += new System.EventHandler(this.checkBoxTuesday_CheckedChanged);
+            // 
+            // checkBoxWednesday
+            // 
+            this.checkBoxWednesday.AutoSize = true;
+            this.checkBoxWednesday.Location = new System.Drawing.Point(16, 130);
+            this.checkBoxWednesday.Name = "checkBoxWednesday";
+            this.checkBoxWednesday.Size = new System.Drawing.Size(57, 17);
+            this.checkBoxWednesday.TabIndex = 4;
+            this.checkBoxWednesday.Text = "Среда";
+            this.checkBoxWednesday.UseVisualStyleBackColor = true;
+            this.checkBoxWednesday.CheckedChanged += new System.EventHandler(this.checkBoxWednesday_CheckedChanged);
+            // 
+            // checkBoxThursday
+            // 
+            this.checkBoxThursday.AutoSize = true;
+            this.checkBoxThursday.Location = new System.Drawing.Point(16, 163);
+            this.checkBoxThursday.Name = "checkBoxThursday";
+            this.checkBoxThursday.Size = new System.Drawing.Size(68, 17);
+            this.checkBoxThursday.TabIndex = 5;
+            this.checkBoxThursday.Text = "Четверг";
+            this.checkBoxThursday.UseVisualStyleBackColor = true;
+            this.checkBoxThursday.CheckedChanged += new System.EventHandler(this.checkBoxThursday_CheckedChanged);
+            // 
+            // checkBoxFriday
+            // 
+            this.checkBoxFriday.AutoSize = true;
+            this.checkBoxFriday.Location = new System.Drawing.Point(16, 198);
+            this.checkBoxFriday.Name = "checkBoxFriday";
+            this.checkBoxFriday.Size = new System.Drawing.Size(69, 17);
+            this.checkBoxFriday.TabIndex = 6;
+            this.checkBoxFriday.Text = "Пятница";
+            this.checkBoxFriday.UseVisualStyleBackColor = true;
+            this.checkBoxFriday.CheckedChanged += new System.EventHandler(this.checkBoxFriday_CheckedChanged);
+            // 
+            // checkBoxSaturday
+            // 
+            this.checkBoxSaturday.AutoSize = true;
+            this.checkBoxSaturday.Location = new System.Drawing.Point(16, 233);
+            this.checkBoxSaturday.Name = "checkBoxSaturday";
+            this.checkBoxSaturday.Size = new System.Drawing.Size(67, 17);
+            this.checkBoxSaturday.TabIndex = 7;
+            this.checkBoxSaturday.Text = "Суббота";
+            this.checkBoxSaturday.UseVisualStyleBackColor = true;
+            this.checkBoxSaturday.CheckedChanged += new System.EventHandler(this.checkBoxSaturday_CheckedChanged);
+            // 
+            // checkBoxSunday
+            // 
+            this.checkBoxSunday.AutoSize = true;
+            this.checkBoxSunday.Location = new System.Drawing.Point(16, 270);
+            this.checkBoxSunday.Name = "checkBoxSunday";
+            this.checkBoxSunday.Size = new System.Drawing.Size(93, 17);
+            this.checkBoxSunday.TabIndex = 8;
+            this.checkBoxSunday.Text = "Воскресенье";
+            this.checkBoxSunday.UseVisualStyleBackColor = true;
+            this.checkBoxSunday.CheckedChanged += new System.EventHandler(this.checkBoxSunday_CheckedChanged);
+            // 
+            // buttonShow
+            // 
+            this.buttonShow.Location = new System.Drawing.Point(16, 307);
+            this.buttonShow.Name = "buttonShow";
+            this.buttonShow.Size = new System.Drawing.Size(93, 36);
+            this.buttonShow.TabIndex = 9;
+            this.buttonShow.Text = "Вывести";
+            this.buttonShow.UseVisualStyleBackColor = true;
+            this.buttonShow.Click += new System.EventHandler(this.buttonShow_Click);
+            // 
+            // FormTimetable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 361);
+            this.Controls.Add(this.buttonShow);
+            this.Controls.Add(this.checkBoxSunday);
+            this.Controls.Add(this.checkBoxSaturday);
+            this.Controls.Add(this.checkBoxFriday);
+            this.Controls.Add(this.checkBoxThursday);
+            this.Controls.Add(this.checkBoxWednesday);
+            this.Controls.Add(this.checkBoxTuesday);
+            this.Controls.Add(this.checkBoxMonday);
+            this.Controls.Add(this.listBoxTimetable);
+            this.Controls.Add(this.labelWeekDay);
+            this.Name = "FormTimetable";
+            this.Text = "Расписание";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelWeekDay;
+        private System.Windows.Forms.ListBox listBoxTimetable;
+        private System.Windows.Forms.CheckBox checkBoxMonday;
+        private System.Windows.Forms.CheckBox checkBoxTuesday;
+        private System.Windows.Forms.CheckBox checkBoxWednesday;
+        private System.Windows.Forms.CheckBox checkBoxThursday;
+        private System.Windows.Forms.CheckBox checkBoxFriday;
+        private System.Windows.Forms.CheckBox checkBoxSaturday;
+        private System.Windows.Forms.CheckBox checkBoxSunday;
+        private System.Windows.Forms.Button buttonShow;
+    }
+}
+
---
+++ Timetable/FormTimetable.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timetable
+{
+    public partial class FormTimetable : Form
+    {
+        string[] raspisanie = { "", "", "", "", "", "", "" };
+        
+        public FormTimetable()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBoxMonday_CheckedChanged(object sender, EventArgs e)
+        {
+            if (raspisanie[0] == "")
+            {
+                raspisanie[0] = "Понедельник - 3 пары"; ;
+            }
+            else
+            {
+                raspisanie[0] = "";
+            }
+        }
+
+        private void checkBoxTuesday_CheckedChanged(object sender, EventArgs e)
+        {
+            if (raspisanie[1] == "")
+            {
+                raspisanie[1] = "Вторник - 3 пары";
+            }
+            else
+            {
+                raspisanie[1] = "";
+            }
+        }
+
+        private void checkBoxWednesday_CheckedChanged(object sender, EventArgs e)
+        {
+            if (raspisanie[2] == "")
+            {
+                raspisanie[2] = "Среда - 4 пары";
+            }
+            else
+            {
+                raspisanie[2] = "";
+            }
+        }
+
+        private void checkBoxThursday_CheckedChanged(object sender, EventArgs e)
+        {
+            if (raspisanie[3] == "")
+            {
+                raspisanie[3] = "Четверг - 4 пары";
+            }
+            else
+            {
+                raspisanie[3] = "";
+            }
+        }
+
+        private void checkBoxFriday_CheckedChanged(object sender, EventArgs e)
+        {
+            if (raspisanie[4] == "")
+            {
+                raspisanie[4] = "Пятница - 3 пары";
+            }
+            else
+            {
+                raspisanie[4] = "";
+            }
+        }
+
+        private void checkBoxSaturday_CheckedChanged(object sender, EventArgs e)
+        {
+            if (raspisanie[5] == "")
+            {
+                raspisanie[5] = "Суббота - 1 пара";
+            }
+            else
+            {
+                raspisanie[5] = "";
+            }
+        }
+
+        private void checkBoxSunday_CheckedChanged(object sender, EventArgs e)
+        {
+            if (raspisanie[6] == "")
+            {
+                raspisanie[6] = "Воскресенье - выходной";
+            }
+            else
+            {
+                raspisanie[6] = "";
+            }
+        }
+
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            listBoxTimetable.Items.Clear();
+            for (int i = 0; i < raspisanie.Length; i++)
+            {
+                if(raspisanie[i] != "")
+                {
+                    listBoxTimetable.Items.Add(raspisanie[i]);
+                }
+            }
+        }
+    }
+}
